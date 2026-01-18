4803e738bfab1d893089ee28185b620d2b7b817b
Ismaeel77 <abozahra.ismaeel@yahoo.com>
2026-01-18 14:35:42 +0200

    update products page

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,9 +1,13 @@
 import useProducts from "../../Hooks/useProducts";
 import { Link } from "react-router-dom";
 import { PacmanLoader } from "react-spinners";
+import { CartContext } from "../../Context/CartContext";
+import { useContext } from "react";
+import toast from "react-hot-toast";
 
 function Products() {
   let { data, isError, error, isLoading } = useProducts();
+  let {addToCart, setCart} = useContext(CartContext)
 
   if (isError) {
     console.log(error);
@@ -15,6 +19,24 @@ function Products() {
         <PacmanLoader color="#615fff" />
       </div>
     );
+  }
+
+  async function addProductToCart(productId) {
+    let response = await addToCart(productId);
+      if (response.data.status === 'success') {
+      console.log(response.data);
+      setCart(response.data)
+      toast.success(response.data.message,{
+        duration:1500,
+        position:'bottom-left',
+        style : {
+          backgroundColor:`#615fff`,
+          color:'white'
+        }
+      })
+    } else {
+      return toast.error(response.data.message)
+    }
   }
 
   return (
@@ -51,8 +73,9 @@ function Products() {
             <button
               type="button"
               className="mt-2 cursor-pointer w-full text-white rounded-md bg-indigo-500 hover:bg-indigo-700 transition-colors px-4 py-2"
+              onClick={() => {addProductToCart(product?.id)}}
             >
-              add to cart
+              Add to cart
             </button>
           </div>
         ))}
